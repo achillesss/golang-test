400ca41e4b2cf8b3c6ebe917d436a9b8f89a17dd
Achillesss <achillesss@126.com>
2019-07-26 16:27:37 +0800

    update

---
+++ tests/UseVariables_test.go
@@ -0,0 +1,49 @@
+package golangtest
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func useVariables(someVariables interface{}) {
+	var val = reflect.ValueOf(someVariables)
+	var typ = reflect.TypeOf(someVariables)
+
+	fmt.Printf("use variables: %+#v, %v", someVariables, typ)
+
+	switch typ.Kind() {
+	case reflect.Interface, reflect.Ptr:
+		fmt.Printf(", element: %v", val.Elem())
+		if val.Elem().CanAddr() {
+			fmt.Printf(", addr: %x", val.Elem().Addr())
+		}
+	default:
+		var addr = &someVariables
+		fmt.Printf(", inner addr: %p", addr)
+	}
+
+	println()
+}
+
+func TestUseVariables(t *testing.T) {
+	var slice = []string{}
+	fmt.Printf("variables: %+[1]v, %[1]T, %p\n", slice, &slice)
+	useVariables(slice)
+	useVariables(&slice)
+
+	var num = 3.1415926
+	fmt.Printf("variables: %+[1]v, %[1]T, %p\n", num, &num)
+	useVariables(num)
+	useVariables(&num)
+
+	var name = "Jason"
+	fmt.Printf("variables: %+[1]v, %[1]T, %p\n", name, &name)
+	useVariables(name)
+	useVariables(&name)
+
+	var alpha = 'Y'
+	fmt.Printf("variables: %+[1]v, %[1]T, %p\n", alpha, &alpha)
+	useVariables(alpha)
+	useVariables(&alpha)
+}
