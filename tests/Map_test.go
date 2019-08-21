9ac96c0c03efa4861acffd3666abcde682d8e949
Achillesss <achillesss@126.com>
2019-08-21 15:56:48 +0800

    add CopyMap

--- tests/Map_test.go
+++ tests/Map_test.go
@@ -41,6 +41,54 @@ func UpdateMapValueMapValue(m, k0, k1, v interface{}) {
 	va.SetMapIndex(key1, value)
 }
 
+func initMap(src interface{}) bool {
+	var val = reflect.ValueOf(src)
+	if val.Kind() != reflect.Ptr {
+		if val.Kind() == reflect.Map {
+			return true
+		}
+		return false
+	}
+
+	if val.Kind() == reflect.Ptr {
+		var elm = val.Elem()
+		if elm.Kind() != reflect.Map {
+			return false
+		}
+		elm.Set(reflect.MakeMap(elm.Type()))
+	}
+	return true
+}
+
+func CopyMap(src, dst interface{}) bool {
+	if !initMap(dst) {
+		return false
+	}
+
+	var vals = reflect.ValueOf(src)
+	var vald = reflect.Indirect(reflect.ValueOf(dst))
+	var typs = vals.Type()
+	var typd = vald.Type()
+
+	if typs.Kind() != reflect.Map && typs.Kind() != typd.Kind() {
+		return false
+	}
+
+	var iter = vals.MapRange()
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
+		t := v.Type()
+		if t.Kind() == reflect.Map {
+			var km = reflect.MakeMap(t)
+			CopyMap(v.Interface(), km.Interface())
+			v = km
+		}
+		vald.SetMapIndex(k, v)
+	}
+	return true
+}
+
 func TestUpdateMapValue(t *testing.T) {
 	var map0 = make(map[int]int)
 	fmt.Printf("map: %+#v\n", map0)
@@ -60,4 +108,15 @@ func TestUpdateMapValue(t *testing.T) {
 	fmt.Printf("map: %+#v\n", map2)
 	inner_map2[1] = 1
 	fmt.Printf("map: %+#v\n", map2)
+
+	var src = make(map[int]map[int]int)
+	src[1] = map[int]int{1: 1}
+	// var dst = make(map[string]string)
+	var dst map[int]map[int]int
+	fmt.Printf("src: %+v, dst: %+v\n", src, dst)
+	// initMap(dst)
+	CopyMap(src, &dst)
+	fmt.Printf("src: %+v, dst: %+v\n", src, dst)
+	// 	dst["y"] = "y"
+	// 	fmt.Printf("src: %+v, dst: %+v\n", src, dst)
 }
