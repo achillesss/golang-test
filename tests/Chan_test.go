7fb901b18d1041711282d961001354dd5cd53785
achillesss <achillesss.cn@gmail.com>
2022-04-24 11:46:34 +0800

    update chan test

--- tests/Chan_test.go
+++ tests/Chan_test.go
@@ -1,6 +1,7 @@
 package golangtest
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
@@ -59,5 +60,74 @@ func TestChan(t *testing.T) {
 	testCloseChan(closeTest)
 	close(closeTest)
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second)
+}
+
+type pauseTest struct {
+	ctx         context.Context
+	cancel      context.CancelFunc
+	pauseChan   chan struct{}
+	restoreChan chan struct{}
+}
+
+func (p *pauseTest) run() {
+	for {
+		select {
+		case <-p.ctx.Done():
+			fmt.Printf("exit\n")
+			return
+
+		case <-p.pauseChan:
+			fmt.Printf("paused.\n")
+			select {
+			case <-p.ctx.Done():
+			case <-p.restoreChan:
+				fmt.Printf("restored.\n")
+			}
+
+		default:
+			fmt.Printf("work.\n")
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
+}
+
+func (p *pauseTest) pause() {
+	fmt.Printf("PAUSE\n")
+	select {
+	case <-p.pauseChan:
+		fmt.Printf("already paused.\n")
+	default:
+		close(p.pauseChan)
+		p.restoreChan = make(chan struct{})
+	}
+}
+
+func (p *pauseTest) restore() {
+	fmt.Printf("RESOTRE\n")
+	select {
+	case <-p.restoreChan:
+		fmt.Printf("already stored.\n")
+	default:
+		close(p.restoreChan)
+		p.pauseChan = make(chan struct{})
+	}
+}
+
+func TestChanPause(t *testing.T) {
+	var p pauseTest
+	p.ctx, p.cancel = context.WithCancel(context.Background())
+	p.restoreChan = make(chan struct{})
+	p.pauseChan = make(chan struct{})
+	go p.run()
+	p.restore()
+	time.Sleep(time.Second)
+	p.pause()
+	time.Sleep(time.Second)
+	p.restore()
+	time.Sleep(time.Second)
+	p.pause()
+	time.Sleep(time.Second)
+	p.cancel()
+	time.Sleep(time.Second)
 }
